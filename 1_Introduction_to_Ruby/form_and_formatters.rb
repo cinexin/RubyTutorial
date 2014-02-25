abf00edfbd33691c90e7072eea9a2feeaa91200d
Migue Guillamón <migueguillamon@gmail.com>
2014-02-25 19:31:23 +0100

    First exercise (Introduction to Ruby)

---
+++ 1_Introduction_to_Ruby/form_and_formatters.rb
@@ -0,0 +1,20 @@
+# Exercise 2 - Introduction to Ruby
+
+print "What's your first name? "
+first_name  = gets.chomp
+first_name.capitalize!
+# this is equivalent to first_name = first_name.capitalize
+
+print "What's your last name? "
+last_name = gets.chomp
+last_name.capitalize!
+
+print "What's your city? "
+city = gets.chomp
+city.capitalize!
+
+print "What's your state? "
+state = gets.chomp
+state.upcase!
+
+puts "Your name is #{first_name} #{last_name}, you live in #{city} located in #{state}"
