3f5baeb956724e2ba5130874319dd9802b5e7d33
Migue Guillamón <migueguillamon@gmail.com>
2014-02-25 19:12:59 +0100

    first commit

---
+++ 1_Introduction_to_Ruby/intro.rb
@@ -0,0 +1,38 @@
+# Declaring variables in Ruby is as simple as this...
+# The convention is always lower case letters and words separated by "_"
+
+my_num = 100    
+
+my_boolean = true     
+
+my_string = "Ruby"  
+
+my_name = "Migue"
+my_age = 32  
+
+# print and puts are commands to show messages on console...
+puts my_num
+print "What's up?"
+puts " Migue"
+
+# some maths...
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 / 17
+power = 2 ** 2
+
+# "." is the notation for calling methods in Ruby 
+# for example, the methods "length" or "reverse" of the "string" object
+puts "Migue".length
+puts "Migue".reverse
+
+# more examples of string methods...
+puts "migue".upcase
+puts "Migue".downcase
+
+=begin
+i'm a multiline
+comment
+ok?
+=end
+
---
+++ README.md
@@ -0,0 +1,10 @@
+README
+
+This is a compilation of the source code of most of the exercises and practises of the Ruby Tutorial by CodeAcademy.com ( www.codeacademy.com ).
+
+I've organised it in folders like the tutorial, and you can see the code commented for education purposes (I'm a Ruby beginner, and this is intended for beginners)
+
+Hope it's helpful
+
+migueguillamon@gmail.com
+http://twitter.com/cineexin
