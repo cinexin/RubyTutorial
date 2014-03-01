6d52305cfac4e7c907d49af87c0eab813aa89ae5
VMWareLinux <migue@ubuntu.(none)>
2014-03-01 12:30:19 +0100

    Ordering your library exercise

---
+++ 5_Methods_block_and_sorting/ordering_your_library.rb
@@ -0,0 +1,8 @@
+# Exercise "Ordering your Library"
+
+=begin
+	the parameter "rev=false" means that the parameter is optional and, in case the user doesn't pass it, it's false by default
+=end
+def alphabetize (arr, rev=false)
+	
+end
