64b07aea862c50764fa74afa376dd6518c61246c
VMWareLinux <migue@ubuntu.(none)>
2014-03-01 12:44:12 +0100

    Ordering your library exercise

--- 5_Methods_block_and_sorting/ordering_your_library.rb
+++ 5_Methods_block_and_sorting/ordering_your_library.rb
@@ -4,5 +4,12 @@
 	the parameter "rev=false" means that the parameter is optional and, in case the user doesn't pass it, it's false by default
 =end
 def alphabetize (arr, rev=false)
-	
+    arr.sort!
+    if (rev==true)
+        arr.reverse!
+    end
+	return arr
 end
+
+numbers = [4,6,7,10,3]
+puts alphabetize(numbers, true)
